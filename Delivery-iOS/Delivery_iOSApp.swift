68fe4e74a583551825c3b8011d17f407c3fd6d2b
fasoxy-it <fasoxy@gmail.com>
2021-04-12 12:42:00 +0200

    Initial Commit

---
+++ Delivery-iOS/Delivery_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delivery_iOSApp.swift
+//  Delivery-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Delivery_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
